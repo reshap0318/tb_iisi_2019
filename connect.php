1bc0576f434a1cf11cb59dbd0a925c06627dea85
Reinaldo Shandev Pratama <aldo.reshap@gmail.com>
2019-11-24 17:26:02 +0700

    first commit

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user = "postgres";
 	$pass = "root";
-	$port = "5434";
+	$port = "5433";
 	$dbname = "bkt_tourism";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
